10ff906ad53d91ffa05825a1dcc17fe1ba4b8e4b
iris <iriskurien@gmail.com>
2025-01-14 13:59:28 +0530

    my chrome thinks im sus

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 service = Service(r"C:\Users\dilip\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe")
@@ -9,8 +11,10 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://discord.com/channels/688852585071116289/1059941605110468668")
 driver.implicitly_wait(60)
 
-latest_link = driver.find_element(By.XPATH, '//*[@id="message-content-1328589214752833559"]/a/span')  
-latest_link.click()
+event_link = WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.XPATH, "//a[contains(@href, 'https://events.mlh.io/events/')]"))
+)
+event_link.click()
 
 time.sleep(15)
 print(driver.title)
